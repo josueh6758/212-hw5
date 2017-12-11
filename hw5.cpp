7e30322db4d93bc73496977ba3ea111f8f525480
Yukkee chang <yukkeechang@gmail.com>
2017-12-11 16:08:04 -0500

    -

--- hw5.cpp
+++ hw5.cpp
@@ -19,27 +19,15 @@ template<class T>
      void set_data(const T& new_data) { m_val = new_data; }
      void set_next_link(Node<T>* new_link)             { m_next = new_link; }
      void set_prev_link(Node<T>* new_link)             { m_prev = new_link; }
-     T get_data() { return m_val;}
+     T& get_data() { return m_val;}
      const Node<T>* get_next_link( ) const { return m_next;}
      Node<T>* get_next_link( )             { return m_next; }
      //we need two for next and prev	
      const Node<T>* get_prev_link( ) const { return m_prev;}
      Node<T>* get_prev_link( )             { return m_prev; }
-     Node<T>* list_search(Node<T>*, T&);
-     void list_head_remove(Node<T>*& );
+       
     
    };
-
-    template<class T>
-    void list_head_remove(Node<T>*& head_ptr) {
-        //pre-condition: takes in head pointer as parameter
-        //post-condition: creates a node pointer that is set to the head pointer, sets head pointer to point to the  next link, then deletes placeholder pointer
-        //not sure if = operator works
-        Node<T> *remove_ptr;
-        remove_ptr = head_ptr;
-        head_ptr = head_ptr->get_next_link( );
-        delete remove_ptr;
-    }
 
     template<class T>
     void list_insert(Node<T>* left_ptr, const T& entry){
@@ -57,11 +45,18 @@ template<class T>
     void list_remove(Node<T>* removed_ptr){
     //PRE-CONDITION: pass in the node you want to remove of the list!!!!!!
         
+    //set the left pointer to the node before the one you're about to delete
     Node<T>* left_ptr = removed_ptr->get_prev_link();
-    Node<T>* right_ptr = removed_ptr->get_prev_link();//left+right node is grabbed using current Node!!!
+    //set the right pointer to the node after the one you're about to delete
+    Node<T>* right_ptr = removed_ptr->get_next_link();//left+right node is grabbed using current Node!!!
+    //set the next link of the left node (left of node that removeptr is pointing at) to node that right_ptr is pointing at
     left_ptr->set_next_link(right_ptr);
+    //set the prev link of the right node (right of the node that removeptr is pointing at) to node that left_ptr is pointing at
     right_ptr->set_prev_link(left_ptr);
+    //now the left and right node's links are no longer set to remove pointer's so the node can be deleted and the pointer can be set to NULL
     delete removed_ptr;
+    removed_ptr = NULL;
+    
 
     }
 
@@ -83,13 +78,13 @@ template<class T>
     template<class T>
     Node<T>* list_search(Node<T>* head_ptr, T& target)
     {
-        Node<T> *cursor;
-        
-        for (cursor = head_ptr; cursor != NULL; cursor = cursor->get_next_link())
-            // the == operator doesn't work atm
-            if (target == cursor->get_data())
-                return cursor;
-        return NULL;
+        return head_ptr;
+        /*Node<T> *cursor;
+        cursor = head_ptr;
+        do {
+            if (target.num_ssn() == cursor->get_data().num_ssn()) return cursor;
+            cursor = cursor->get_next_link();
+        }while(cursor != head_ptr);*/
     }
 
 //-----------------------------SortedBag Header
@@ -99,11 +94,9 @@ template<class T>
    public:
      SortedBag() : m_data(0), m_size(0), m_asc(true), m_curr(0) {}
      //copy constructor
-     SortedBag(const SortedBag& sb) {m_data=sb.m_data; m_size=sb.m_size; m_asc=sb.m_asc; m_curr=sb.m_curr;}
-
+       SortedBag(const SortedBag& sb) {m_data=sb.m_data; m_size=sb.m_size; m_asc=sb.m_asc; m_curr=sb.m_curr;}
      void operator =(const SortedBag&);
-     //~SortedBag();
-
+       //~SortedBag();
      bool erase_one(const T&);
      long erase(const T&);
      void insert(const T&);
@@ -122,6 +115,7 @@ template<class T>
      void operator--(){m_data = m_data->get_prev_link();}
      bool boolforequal(SortedBag<T>&, SortedBag<T>&);
      T get() {return m_data->get_data();};
+     void remove(const T&);
      
 //   friend bool operator<(const Node<T> &,const Node<T> &);
 
@@ -146,7 +140,20 @@ template<class T>
 
 
 //-----------------------------------SortedBag Declaration
-
+   template<class T>
+    long SortedBag<T>::erase(const T& student) {
+        if(student.num_ssn()==head_ptr->get_data().num_ssn()) {
+            Node<T>* curr=head_ptr;
+            head_ptr = head_ptr.get_next_link();
+            delete curr;
+            curr = NULL;
+            --m_size;
+            return true;
+        } else {
+            
+            list_remove(student);
+        }
+    }
    template<class T>
    SortedBag<T> operator+(const SortedBag<T>&, const SortedBag<T>&);
 
@@ -244,37 +251,37 @@ Notice!!!! i am not using pointers but actual nodes so use .get rather than
     {
         //make a node type pointer to target
         Node<T> *target_ptr;
-        target_ptr = list_search(head_ptr, target);
+      //  target_ptr = list_search(head_ptr,target);
         if (target_ptr == NULL)
             return false; // target isn't in the bag, so no work to do
         target_ptr->set_data( head_ptr->get_data() );
-        list_head_remove(head_ptr);
+        remove(head_ptr->get_data());
         --m_size;
         return true;
         
     }
-    template<class T>
-    long SortedBag<T>::erase(const T& target)
-    {
-        int answer = 0;
-        //make a node type pointer to target
-        Node<T> *target_ptr;
-        target_ptr = list_search(head_ptr, target);
-        while (target_ptr != NULL)
-        {
-            // Each time that target_ptr is not NULL, we have another occurrence
-            // of target. We remove this target using the same technique that
-            // was used in erase_one.
-            target_ptr->set_data( head_ptr->get_data( ) );
-            target_ptr = target_ptr->get_next_link( );
-            target_ptr = list_search(target_ptr, target);
-            list_head_remove(head_ptr);
-            --m_size;
-            ++answer;
-        }
-        return answer;
-        
-    }
+//    template<class T>
+//    long SortedBag<T>::erase(const T& target)
+//    {
+//        int answer = 0;
+//        //make a node type pointer to target
+//        Node<T> *target_ptr;
+//       // target_ptr = list_search(head_ptr, target);
+//        while (target_ptr != NULL)
+//        {
+//            // Each time that target_ptr is not NULL, we have another occurrence
+//            // of target. We remove this target using the same technique that
+//            // was used in erase_one.
+//            target_ptr->set_data( head_ptr->get_data( ) );
+//            target_ptr = target_ptr->get_next_link( );
+//           // target_ptr = list_search(target_ptr, target);
+//            remove(head_ptr->get_data());
+//            --m_size;
+//            ++answer;
+//        }
+//        return answer;
+//
+//    }
     template<class T>
     long SortedBag<T>::count(const T& t) const
     {
@@ -348,7 +355,7 @@ Notice!!!! i am not using pointers but actual nodes so use .get rather than
        for(int i=0; i<linked_list.size();i++){
            cout<<"remove the first node of the 1st list "<<linked_list.get().string_ssn()<<endl;
        }
-       
+
        list_2.erase_one(linked_list.beginPointer()->get_data());
        for(int i=0; i<list_2.size();i++){
            cout<<"remove the first node of the 2nd list "<<list_2.get().string_ssn()<<endl;
@@ -360,7 +367,7 @@ Notice!!!! i am not using pointers but actual nodes so use .get rather than
                cout<<"remove the last node of the 1st list "<<linked_list.get().string_ssn()<<endl;
            }
        }
-       
+
        //While list_2 is at the end
        while(list_2.end()) {
            list_2.erase(linked_list.endPointer()->get_data());
@@ -368,21 +375,21 @@ Notice!!!! i am not using pointers but actual nodes so use .get rather than
             cout<<"remove the last node of the 2st list "<<linked_list.get().string_ssn()<<endl;
            }
        }
+
+//       for(int i=0; i<list_2.size(); i++){
+//           linked_list.erase(++linked_list);
+//           //check if size of list is 0
+//           if(linked_list.size() == 0) {
+//           //Alt method but has errors because of data types:
+//           //check if the data pointed by head pointer is null
+//           //if(linked_list.beginPointer()->get_data == NULL) {
+//               cout << "list empty" << endl;
+//           } else {
+//               //what was not removed in linked_list
+//               cout<<"empty linked_list from list_2: "<<linked_list.get().string_ssn()<<endl;
+//           }
        
-       for(int i=0; i<list_2.size(); i++){
-           linked_list.erase(++linked_list);
-           //check if size of list is 0
-           if(linked_list.size() == 0) {
-           //Alt method but has errors because of data types:
-           //check if the data pointed by head pointer is null
-           //if(linked_list.beginPointer()->get_data == NULL) {
-               cout << "list empty" << endl;
-           } else {
-               //what was not removed in linked_list
-               cout<<"empty linked_list from list_2: "<<linked_list.get().string_ssn()<<endl;
-           }
-           
-       }
+ //      }
        
        
        
